8ec7c8cf46cff0c054d92746f2850e541922465c
Nicholas Spencer <ngspencer97@outlook.com>
2018-07-15 01:03:44 -0400

    Adding style to contact form and changing some font

--- contact.php
+++ contact.php
@@ -1,5 +1,16 @@
 <? php
 
 if (isset($_POST['submit'])) {
+	$name = $_POST['name']:
+	$company = $_POST['company'];
+	$mailFrom = $_POST['email'];
+	$subject = $_POST['subject'];
+	$message = $_POST['message'];
 
+	$mailTo = "ngspencer97@outlook.com";
+	$headers = "From: ".$mailFrom;
+	$txt = "You have received an e-mail from ".$name.".\n\n".$message;
+
+	mail($mailTo, $subject, $txt, $headers);
+	header("Location: contact.html?mailsend");
 }
